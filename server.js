fbfc505399e36b22008287c5399f62466ead6903
@wwwjim <send@jimmy.email>
2019-09-03 18:53:25 -0700

    structure: adds root page adds nodemon

--- server.js
+++ server.js
@@ -9,21 +9,6 @@ const port = process.env.PORT || 1337;
 const app = next({ dev, quiet: false });
 const nextRequestHandler = app.getRequestHandler();
 
-const createParams = ({ req, page, data }) => {
-  let userAgent = req.headers['user-agent'] || '';
-
-  return Object.assign(
-    {
-      page,
-      platform: userAgent.match(/Mac OS X/) ? 'apple' : 'windows',
-      supported: !!userAgent.match(/Mac OS X/) || !!userAgent.match(/Windows/),
-      data,
-    },
-    req.params,
-    req.query
-  );
-};
-
 app.prepare().then(() => {
   const server = express();
 
